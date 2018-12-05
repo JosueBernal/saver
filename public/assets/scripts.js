4f9de41990b5efda3ed9d131bbf3d31e6a385a09
Jos Hernandez <josue.bernal.1964@gmail.com>
2018-12-05 01:12:48 -0600

    Avance de página Saver sin js

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,3 +1,6 @@
 particlesJS.load('particles', 'assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+particlesJS.load('particles2', 'assets/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
